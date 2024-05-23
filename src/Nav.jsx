6c4dde1286f3e1ed9b8786a35db43be58a343463
minhazapon <minhajulabedinapon@gmail.com>
2024-05-23 07:08:20 -0700

    added files

---
+++ src/Nav.jsx
@@ -0,0 +1,60 @@
+import { NavLink } from "react-router-dom";
+
+
+
+
+
+const Nav = () => {
+
+    const links = <>
+    
+         <li><NavLink to="/" >Home</NavLink></li>
+         <li><NavLink to="/about" >About</NavLink></li>
+         <li><NavLink to="/appo" >Appointment</NavLink></li>
+    
+    
+    
+    </>
+
+
+
+    return (
+        <div>
+
+<div className="navbar bg-base-100">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </div>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+       
+        {links}
+
+
+      </ul>
+    </div>
+
+    <div className=" flex items-center gap-1">
+    <img className=" h-[40px]" src="https://i.ibb.co/nPHNzLj/diabetes.png" alt="" />
+    <a className=" text-2xl font-bold    text-blue-500"><span className=" text-red-500">Health</span> Talk</a>
+    </div>
+    {/* text-[#1a504a] */}
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+    
+      {links}
+
+    </ul>
+  </div>
+  <div className="navbar-end">
+    <a className="btn">Login</a>
+  </div>
+</div>
+            
+        </div>
+    );
+};
+
+export default Nav;
