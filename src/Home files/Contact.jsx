e7741f5fede289a9ee485bc996c8bf9ee72ec0ff
minhazapon <minhajulabedinapon@gmail.com>
2024-05-24 07:06:39 -0700

    contact file

---
+++ src/Home files/Contact.jsx
@@ -0,0 +1,65 @@
+
+
+
+
+const Contact = () => {
+    return (
+        <div className=" mb-10 mt-20 ml-10 mr-10">
+
+
+           <div>
+
+
+            <div>
+
+                <h1 className=" text-5xl font-bold">Contact With Us</h1>
+                <p className=" mt-5 text-xl text-slate-500">Find the contact information for <br></br>
+                the doctor or medical practice.<br></br>
+                 This can usually be found on their<br></br>
+                  official website, through your<br></br>
+                   health insurance provider,<br></br>
+                    or via an online directory.</p>
+
+
+                 <p className=" flex items-center mt-5 gap-1">
+                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 3.75v4.5m0-4.5h-4.5m4.5 0-6 6m3 12c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21 12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z" />
+                  </svg>
+                   <p className=" text-xl">01950374409</p>  
+                </p>
+
+                 <p className=" flex items-center mt-5 gap-1">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                   <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                   <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                  </svg>
+                   <p className=" text-xl">New York, America</p>  
+                </p>
+
+                   
+
+
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+           </div>
+            
+        </div>
+    );
+};
+
+export default Contact;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,6 @@
 import Address from "./Home files/Address";
 import Banner from "./Home files/Banner";
+import Contact from "./Home files/Contact";
 import Review from "./Home files/Review";
 import Service from "./Home files/Service";
 import Expert from "./expert doctor/Expert";
@@ -17,6 +18,7 @@ const Home = () => {
             <Review></Review>
             <Expert></Expert>
             <ExpertCard></ExpertCard>
+            <Contact></Contact>
             
         </div>
     );
