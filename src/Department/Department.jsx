344bc285cf6bf8fd8a977768b5c6ff217e70fb2b
minhazapon <minhajulabedinapon@gmail.com>
2024-05-27 05:07:33 -0700

    depart file

--- src/Department/Department.jsx
+++ src/Department/Department.jsx
@@ -1,16 +1,49 @@
+import { useEffect, useState } from "react";
+import DEpartDetails from "./DEpartDetails";
 
 
 
 const Department = () => {
 
+      
+     const [depart, setDepart] = useState([])
 
 
+     useEffect( () => {
+
+      fetch('departments.json')
+      .then(res => res.json())
+      .then(data => setDepart(data))
+
+
+     } , [])
 
 
 
     
     return (
-        <div>
+        <div className=" mb-10 mt-10 mr-10 ml-10">
+
+            <div>
+
+               <div className=" bg-blue-100 p-2 rounded-xl flex-col md:flex-row lg:flex-row text-center">
+                <h1 className="  text-blue-500 text-4xl font-bold">Health Talk working on ten Category Departments {depart.length} </h1>
+                <p className=" text-xl text-slate-500 mt-5">Illness is the doctor to whom we pay most heed; to kindness, to knowledge, we make promise only; pain we obey.</p>
+               </div>
+
+
+            </div>
+
+
+            <div className=" flex justify-center items-center mb-10 mt-10 ml-10 mr-10 ">
+                <div className=" grid  md:grid-cols-3 gap-10">
+                    {
+                        depart.map( depart => <DEpartDetails  depart={depart}></DEpartDetails> )
+                    }
+                </div>
+            </div>
+
+            
             
         </div>
     );
