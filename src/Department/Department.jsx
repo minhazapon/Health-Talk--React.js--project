35dad92d26df4db5922248a07bd119a0265491d9
minhazapon <minhajulabedinapon@gmail.com>
2024-05-27 05:11:08 -0700

    final depart file

--- src/Department/Department.jsx
+++ src/Department/Department.jsx
@@ -27,7 +27,7 @@ const Department = () => {
             <div>
 
                <div className=" bg-blue-100 p-2 rounded-xl flex-col md:flex-row lg:flex-row text-center">
-                <h1 className="  text-blue-500 text-4xl font-bold">Health Talk working on ten Category Departments {depart.length} </h1>
+                <h1 className="  text-blue-500 text-4xl font-bold">Health Talk working on ten Category Departments  </h1>
                 <p className=" text-xl text-slate-500 mt-5">Illness is the doctor to whom we pay most heed; to kindness, to knowledge, we make promise only; pain we obey.</p>
                </div>
 
@@ -35,10 +35,10 @@ const Department = () => {
             </div>
 
 
-            <div className=" flex justify-center items-center mb-10 mt-10 ml-10 mr-10 ">
+            <div className=" flex justify-center items-center mb-10 mt-10 ">
                 <div className=" grid  md:grid-cols-3 gap-10">
                     {
-                        depart.map( depart => <DEpartDetails  depart={depart}></DEpartDetails> )
+                        depart.map( depart => <DEpartDetails   depart={depart}></DEpartDetails> )
                     }
                 </div>
             </div>
