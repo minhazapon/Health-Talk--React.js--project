a993b92d1593ae784ac90c3d997b8f1344bf4627
minhazapon <minhajulabedinapon@gmail.com>
2024-06-01 03:14:16 -0700

    final file

--- src/Gallery/Slide.jsx
+++ src/Gallery/Slide.jsx
@@ -22,7 +22,7 @@ const Slide = () => {
 
 
     return (
-        <div className=" mb-10 mt-10 ml-10 mr-10">
+        <div className=" mb-10 mt-10 ">
             
              <Marquee>
               <div className=' mb-10 text-center '>
